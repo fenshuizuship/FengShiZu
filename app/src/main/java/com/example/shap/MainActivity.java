4cf0daca7be8be3b26ae211ce3b4fe3700a43160
rlj <2597698564@qq.com>
2019-06-24 20:42:04 +0800

    提交修改

--- app/src/main/java/com/example/shap/MainActivity.java
+++ app/src/main/java/com/example/shap/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.shap;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.example.shap.base.BaseActivity;
-import com.example.shap.base.BaseFragment;
-import com.example.shap.interfaces.IBaseView;
 import com.example.shap.interfaces.contract.MainContracy;
 import com.example.shap.presenter.HomePresenter;
-import com.example.shap.ui.fragment.HomeFragment;
 
 public class MainActivity extends BaseActivity<MainContracy.View,MainContracy.Presenter> implements MainContracy.View {
 
