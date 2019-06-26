de5b907e89bd41bfec20b9ca982992e4cb0bff7d
rlj <2597698564@qq.com>
2019-06-26 21:15:57 +0800

    提交Subjectfragment

---
+++ app/src/main/java/com/example/shap/adaper/SubListAdapter.java
@@ -0,0 +1,82 @@
+package com.example.shap.adaper;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.shap.R;
+import com.example.shap.bean.SubjectListBean;
+
+import java.util.ArrayList;
+
+ public class SubListAdapter extends RecyclerView.Adapter {
+
+    private ArrayList<SubjectListBean.DataBeanX.DataBean> mDataBeans;
+    private Context mContext;
+
+    public SubListAdapter(ArrayList<SubjectListBean.DataBeanX.DataBean> dataBeans) {
+
+        mDataBeans = dataBeans;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        mContext = viewGroup.getContext();
+        View inflate = LayoutInflater.from(mContext).inflate(R.layout.item_subject_list, null);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int i) {
+        ViewHolder viewHolder = (ViewHolder) holder;
+        SubjectListBean.DataBeanX.DataBean model = mDataBeans.get(i);
+        Glide.with(mContext).load(model.getScene_pic_url()).into(viewHolder.mSubjectItemIv);
+        viewHolder.mSubjectItemTv1.setText(model.getTitle());
+        viewHolder.mSubjectItemTv2.setText(model.getSubtitle());
+        viewHolder.mSubjectItemTv3.setText(model.getPrice_info()+"元起");
+
+        int id = model.getId();
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDataBeans.size();
+    }
+
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView mSubjectItemIv;
+        TextView mSubjectItemTv1;
+        TextView mSubjectItemTv2;
+        TextView mSubjectItemTv3;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.mSubjectItemIv = (ImageView) itemView.findViewById(R.id.subject_item_iv);
+            this.mSubjectItemTv1 = (TextView) itemView.findViewById(R.id.subject_item_tv1);
+            this.mSubjectItemTv2 = (TextView) itemView.findViewById(R.id.subject_item_tv2);
+            this.mSubjectItemTv3 = (TextView) itemView.findViewById(R.id.subject_item_tv3);
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/shap/interfaces/Api.java
+++ app/src/main/java/com/example/shap/interfaces/Api.java
@@ -2,6 +2,7 @@ package com.example.shap.interfaces;
 
 import com.example.shap.bean.Bean_search;
 import com.example.shap.bean.IndexBean;
+import com.example.shap.bean.SubjectListBean;
 
 import io.reactivex.Flowable;
 import retrofit2.http.GET;
@@ -17,4 +18,10 @@ public interface Api {
     //http://cdwan.cn:8360/api/goods/list
     @GET("goods/list")
     Flowable<Bean_search> getdata_search();
+
+
+    //============subject==========
+    @GET("topic/list")
+    Flowable<SubjectListBean> getSubjectListBean();
+
 }
